9a43cb1d0ef2063d1170c32551d79deb6e9ec97d
niccokuc <nerminkuc74@gmail.com>
2016-01-23 17:03:04 +0100

    Lecture21

---
+++ joins/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+
+class EmailForm(forms.Form):
+    name = forms.CharField(required=True)
+    email = forms.EmailField(required=True)
--- joins/views.py
+++ joins/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from .forms import EmailForm
+from .models import Join
 
 # how the page is viewed
-
 def home(request):
-    context = {}
+    #print(request.POST)
+    form = EmailForm(request.POST or None)
+    if form.is_valid():
+        email = form.cleaned_data['email']
+        new_join, created = Join.objects.get_or_create(email=email)
+        print(new_join)
+    context = {"form": form}
     template = 'home.html'
     return render(request, template, context)
