3622d2cac0d4ef31c06e89d14942ae60dca087d9
niccokuc <nerminkuc74@gmail.com>
2016-01-23 17:48:46 +0100

    Lecture21

--- joins/forms.py
+++ joins/forms.py
@@ -1,5 +1,22 @@
 from django import forms
+#from django.forms import ModelForm
+from joins.models import Join
 
 class EmailForm(forms.Form):
-    name = forms.CharField(required=True)
+    name = forms.CharField()
     email = forms.EmailField(required=True)
+
+class JoinForm(forms.ModelForm):
+    class Meta:
+        model = Join
+        fields = "__all__" # i had to get this from web search must include all fields.
+
+#
+# >>> from django.forms import ModelForm
+# >>> from myapp.models import Article
+#
+# # Create the form class.
+# >>> class ArticleForm(ModelForm):
+# ...     class Meta:
+# ...         model = Article
+# ...         fields = ['pub_date', 'headline', 'content', 'reporter']
--- joins/views.py
+++ joins/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
-from .forms import EmailForm
+from .forms import JoinForm
 from .models import Join
 
 # how the page is viewed
 def home(request):
     #print(request.POST)
-    form = EmailForm(request.POST or None)
+
+    # This is usign regualr Django forms
+    # form = EmailForm(request.POST or None)
+    # if form.is_valid():
+    #     email = form.cleaned_data['email']
+    #     new_join, created = Join.objects.get_or_create(email=email)
+    #     print(new_join)
+    form = JoinForm(request.POST or None)
     if form.is_valid():
+        new_join = form.save(commit=False)
         email = form.cleaned_data['email']
-        new_join, created = Join.objects.get_or_create(email=email)
-        print(new_join)
+        new_join_old, created = Join.objects.get_or_create(email=email)
+
     context = {"form": form}
     template = 'home.html'
     return render(request, template, context)
